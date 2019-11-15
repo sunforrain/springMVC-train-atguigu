14acd8c64c62980d841a4516573862600d5845bf
shl <shl_glo@sina.com>
2019-11-15 10:33:08 +0800

    springMVC_CRUD的I

--- springmvc-2/src/com/atguigu/springmvc/crud/handlers/EmployeeHandlers.java
+++ springmvc-2/src/com/atguigu/springmvc/crud/handlers/EmployeeHandlers.java
@@ -1,9 +1,13 @@
 package com.atguigu.springmvc.crud.handlers;
 
+import com.atguigu.springmvc.crud.dao.DepartmentDao;
 import com.atguigu.springmvc.crud.dao.EmployeeDao;
+import com.atguigu.springmvc.crud.entities.Department;
+import com.atguigu.springmvc.crud.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Map;
 
@@ -12,6 +16,20 @@ public class EmployeeHandlers {
 
     @Autowired
     private EmployeeDao employeeDao;
+    @Autowired
+    private DepartmentDao departmentDao;
+
+    /**
+     * 跳转到添加页面的目标方法,需要指定method为get,我们是RESTFUL的
+     * @param map
+     * @return
+     */
+    @RequestMapping(value="/emp", method=RequestMethod.GET)
+    public String input(Map<String, Object> map){
+        map.put("departments", departmentDao.getDepartments());
+        map.put("employee", new Employee());
+        return "input";
+    }
 
     /**
      * 简单遍历 employee的list
