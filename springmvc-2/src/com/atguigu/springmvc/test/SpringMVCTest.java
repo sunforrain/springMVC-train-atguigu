66377417a144c9aa912de65f163b88efe0946467
shl <shl_glo@sina.com>
2019-11-19 17:38:12 +0800

    异常处理_ExceptionHandler注解以及@ControllerAdvice标注的类用于集中处理异常

--- springmvc-2/src/com/atguigu/springmvc/test/SpringMVCTest.java
+++ springmvc-2/src/com/atguigu/springmvc/test/SpringMVCTest.java
@@ -42,7 +42,7 @@ public class SpringMVCTest {
     /**
      * 1. 在 @ExceptionHandler 方法的入参中可以加入 Exception 类型的参数, 该参数即对应发生的异常对象
      * 2. @ExceptionHandler 方法的入参中不能传入 Map. 若希望把异常信息传导页面上, 需要使用 ModelAndView 作为返回值
-     * 3. @ExceptionHandler 方法标记的异常有优先级的问题.
+     * 3. @ExceptionHandler 方法标记的异常有优先级的问题.优先用最接近的异常的处理方法
      * 4. @ControllerAdvice: 如果在当前 Handler 中找不到 @ExceptionHandler 方法来出来当前方法出现的异常,
      * 则将去 @ControllerAdvice 标记的类中查找 @ExceptionHandler 标记的方法来处理异常.
      */
