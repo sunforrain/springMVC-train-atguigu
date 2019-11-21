c76e19e24305cba20c5f2e5fae56c7bc70912131
shl <shl_glo@sina.com>
2019-11-21 17:19:15 +0800

    spring与springMVC的搭配及相关配置,两个IOC容器的关系

--- springmvc-3/src/com/atguigu/springmvc/UserService.java
+++ springmvc-3/src/com/atguigu/springmvc/UserService.java
@@ -3,9 +3,14 @@ package com.atguigu.springmvc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * 为了演示springMVC与spring整合
+ * 以及是否会创建两个实例的问题,演示的Service
+ */
 @Service
 public class UserService {
 
+    // springIOC容器中的bean不能引用springMVC IOC容器中的bean,这里能发现是报错的
 	@Autowired
 	private HelloWorld helloWorld;
 	
